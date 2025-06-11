398cd0c169debf7cfb9526dbc21850c1d4db9f38
Danilo <dev.danilotdr@gmail.com>
2025-06-11 15:51:15 -0400

    Finalização do Projeto KANBAN

--- src/javascript/Script.js
+++ src/javascript/Script.js
@@ -1,29 +1,47 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const items = document.querySelectorAll('.kanban-card');
-//     let draggedItem = null;
+document.addEventListener('DOMContentLoaded', () => {
+    const items = document.querySelectorAll('.kanban-card');
+    const columns = document.querySelectorAll('.kanban-cards')
+    let draggedItem = null;
 
-//     items.forEach(item => {
-//         item.addEventListener('dragstart', () => {
-//             draggedItem = item;
-//             item.classList.add('dragging');
-//         });
+    items.forEach(item => {
+        item.addEventListener('dragstart', () => {
+            item.classList.add('dragging');
+            draggedItem = item;
+        });
 
-//         item.addEventListener('dragend', () => {
-//             item.classList.remove('dragging');
-//             document.querySelectorAll('.cards-hover').forEach(el => el.classList.remove('cards-hover'));
-//         });
+        item.addEventListener('dragend', () => {
+            item.classList.remove('dragging'); 
+        });
 
-//         item.addEventListener('dragover', e => {
-//             e.preventDefault();
-//             e.currentTarget.classlist.add('cards-hover');
-//         })
+        item.addEventListener('dragover', e => {
+            e.preventDefault();
+            e.classlist.add('cards-hover');
+        })
 
-//         item.addEventListener('drop', () => {
-//             item.addEventListener('drop', e => {
-//             e.currentTarget.remove('cards-hover');
-//         })
-//     })
-// })})
+        item.addEventListener('drop', () => {
+            item.addEventListener('drop', e => {
+            e.classList.remove('cards-hover');
+        })
+    })
+
+    columns.forEach(column => {
+        column.addEventListener('dragover', (e) => {
+            e.preventDefault();
+            e.currentTarget.classList.add('cards-hover');
+        });
+        
+        column.addEventListener('dragleave', (e) => {
+            e.currentTarget.classList.remove('cards-hover'); 
+        });
+
+        column.addEventListener('drop', (e) => {
+            e.currentTarget.classList.remove('cards-hover'); 
+
+            const dragCard = document.querySelector('.kanban-card.dragging');
+            e.currentTarget.appendChild(dragCard);
+        })
+    })
+})})
 
 
 
@@ -32,11 +50,11 @@
 
 
 // document.querySelectorAll('.kanban-card').forEach(card => {
-//     card.addEventListener('dragstart', (e) => {
+//     card.addEventListener('dragstart', e => {
 //         e.currentTarget.classlist.add('dragging');
 //     })
 
-//     card.addEventListener('dragend', (e) => {
+//     card.addEventListener('dragend', e => {
 //         e.currentTarget.classlist.remove('dragging');
 //     })
 // })
